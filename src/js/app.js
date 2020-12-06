f80851212c2cf284ebc696c6e0b4399a2e0ec93b
Dzmitry Duboyski <dzmitry.duboyski@gmail.com>
2020-12-07 00:53:52 +0300

    feat: received encrypted letters implemented

---
+++ src/js/app.js
@@ -0,0 +1,75 @@
+import {getEncriptingText} from "./getEncriptingText.js";
+ import {characterList} from "./constants/constants.js";
+
+const view = {
+  showEndcriptionText: function (endcriptionText = '') {
+    const el = document.getElementById('endcriptionText');
+    el.innerHTML = endcriptionText;
+  },
+};
+
+const model = {
+  text: '',
+  encripting: function(inputText) {
+   // const inputText = document.querySelector("#textareaNotEncriptionText").value;
+    const isEmptyInputText = !inputText;
+     if(isEmptyInputText) {
+       return alert("введите текст");
+     }
+     this.text = getEncriptingText(inputText).join('; ');
+     return this.text;
+  },
+}
+
+const controller = {
+  handleClick: function () {
+    const inputText = document.querySelector("#textareaNotEncriptionText").value;
+    let result = model.encripting(inputText);
+    view.showEndcriptionText(result);
+
+
+  }
+};
+
+ (function(){
+   const app = {
+     init: function () {
+      this.main();
+      this.event();
+     },
+     main: function () {
+       //someCode
+     },
+     event: function () {
+       const el = document.getElementById('buttonStartEncryptyon');
+       el.onclick = controller.handleClick;
+     },
+   };
+   app.init();
+ }());
+
+
+// const BTNstartEnctypyonText = document.querySelector('#buttonStartEncryptyon');
+// const tempCode = document.querySelector('#endcriptionText');
+
+// const startEncriptingText = () => {
+//   const inputText = document.querySelector("#textareaNotEncriptionText").value;
+//   const isEmptyInputText = !inputText;
+//   if(isEmptyInputText) {
+//     return alert("введите текст");
+//   }
+//   console.log(inputText[0].charCodeAt());
+//   //console.log(getEncriptingText(inputText));
+//   tempCode.textContent = getEncriptingText(inputText).join('; ');
+//   console.log(isSymolIncludedInTheLibrary(inputText[0]));
+// }
+
+//BTNstartEnctypyonText.addEventListener('click',startEncriptingText);
+
+const isSymolIncludedInTheLibrary = (checkSymbol) => {
+  const checkSymbolCode = checkSymbol.charCodeAt();
+  //console.log('checkSymbol= ' + checkSymbol);
+  //console.log(characterList.hasOwnProperty(checkSymbolCode));
+  const isIncluded = characterList.hasOwnProperty(checkSymbolCode);
+  return isIncluded;
+}
--- src/js/getEncriptingText.js
+++ src/js/getEncriptingText.js
@@ -1,4 +1,48 @@
+import {characterList} from './constants/constants.js';
+
+/**
+ * @param {max} - the maximum is equal to the length of the array (arrayAlternativeCharacters)
+ * @returns  returns an integer, from min to max
+ */
+const getRandomIndex = (min = 0, max) => {
+  max = max -1; 
+  let rand = min + Math.random() * (max + 1 - min);
+  console.log('getRandomIndex=' + Math.floor(rand));
+  return Math.floor(rand);
+}
+
+
 export function getEncriptingText(inputText) {
-  const newText = inputText.split('').map((el) => el.charCodeAt())
-  return newText;
+  const arrCodeSymbol = inputText.split('').map((el) => el.charCodeAt())
+  // console.log("arrCodeSymbol: "+arrCodeSymbol);
+  // console.log(arrCodeSymbol);
+
+  const updateText = arrCodeSymbol.map((el)=> {
+    //el - это код символа
+    // console.log(characterList.hasOwnProperty(el));
+    const isIncludedInCharacterList = characterList.hasOwnProperty(el);
+    if(isIncludedInCharacterList){
+      
+      //если символ есть в базе
+      const arrayAlternativeCharacters = characterList[`${el}`];
+      const lengthArrayAlternativeCharacters = Array(...characterList[`${el}`]).length;
+      console.log(lengthArrayAlternativeCharacters);
+      const randomIndex = getRandomIndex(0,lengthArrayAlternativeCharacters);
+      const newCharacter = arrayAlternativeCharacters[randomIndex];
+      console.log(newCharacter);
+      return 'XXX';
+    }
+    return el;
+  })
+
+
+
+  // result = result.split(';')
+  // console.log(result)
+  // console.log(result[0])
+  // console.log(typeof(result))
+  // console.log(isSymolIncludedInTheLibrary(String(result[0])));
+
+  console.log(updateText)
+  return updateText;
 }
