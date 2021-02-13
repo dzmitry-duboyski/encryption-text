22d236ac731693d1c95b093b31363e8c5571b0cf
Dzmitry Duboyski <dzmitry.duboyski@gmail.com>
2021-02-13 03:29:59 +0300

    fead: add webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/app';
+import './style/style.scss';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+console.log('index.js')
---
+++ webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    },
+  }
+  if(isProd){
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+    ]
+  }
+  return config;  
+}
+
+const filename = ext => isDev ? `[name].${ext}` :`[name].[hash].${ext}`;
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill','./index.js'],
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      '@models': path.resolve(__dirname,'src/models'),      
+      '@': path.resolve(__dirname,'src'),      
+    }
+  },
+  optimization: optimization(),
+  devServer: {
+    port:8282,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      // title: 'webpack config' //можно прописать title для созданого файла 
+      template: './index.html',
+
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { 
+          from: path.resolve(__dirname,'src/assets/favicon.ico'),
+          to: path.resolve(__dirname, 'dist') 
+        }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    }),
+  ],
+  module: {
+    rules: [
+       {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          'css-loader',
+        ],
+      },
+       {
+        test: /\.s[ac]ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/i,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/i,
+        use: ['file-loader']
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader']
+      },
+      {
+        test: /\.csv$/i,
+        use: ['csv-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ],
+  },
+}
